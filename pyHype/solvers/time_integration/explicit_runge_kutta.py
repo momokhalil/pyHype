75e1769e2ccbb0296416a9aa97e06e5cd5aa2ba7
Mohamed Khalil <62818656+momokhalil@users.noreply.github.com>
2021-05-21 18:36:01 -0400

    Added some documentation to RK4 and RK2

--- pyHype/solvers/time_integration/explicit_runge_kutta.py
+++ pyHype/solvers/time_integration/explicit_runge_kutta.py
@@ -3,6 +3,16 @@ from pyHype.solvers.time_integration.base import ExplicitRungeKutta
 
 class RK4(ExplicitRungeKutta):
     def __init__(self, inputs, refBLK):
+        """
+        Defines the fourth order explicit Runge-Kutta method for time integrations.
+
+        Butcher Tableau:
+        1/2
+        0	1/2
+        0	0	1
+        1/6	1/3	1/3	1/6
+
+        """
 
         super().__init__(inputs, refBLK)
 
@@ -11,8 +21,16 @@ class RK4(ExplicitRungeKutta):
 
 class RK2(ExplicitRungeKutta):
     def __init__(self, inputs, refBLK):
+        """
+        Defines the fourth order explicit Runge-Kutta method for time integrations.
+
+        Butcher Tableau:
+        1/2
+        0   1
+
+        """
 
         super().__init__(inputs, refBLK)
 
-        self.a = [[0.5]]
+        self.a = [[0.5], [0, 1]]
         self.num_stages = 2
