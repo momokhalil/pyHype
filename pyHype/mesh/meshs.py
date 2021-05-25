4f87e59aff0b4351cd3723a56b485b690927ca2e
Mohamed Khalil <62818656+momokhalil@users.noreply.github.com>
2021-05-24 20:10:47 -0400

    Revert "revert 3"
    This reverts commit de8b091e4a11e417ae0461305bcdc28cfbd1b478.

--- pyHype/mesh/meshs.py
+++ pyHype/mesh/meshs.py
@@ -1,6 +1,7 @@
+from pyHype.mesh import mesh_builder
 
 # Meshes
-def simple_mesh(nx, ny):
+def simple_mesh(nx, ny, n):
     block1 = {'nBLK': 1,
               'NE': [5, 5],
               'NW': [0, 5],
@@ -8,7 +9,7 @@ def simple_mesh(nx, ny):
               'SW': [0, 0],
               'nx': nx,
               'ny': ny,
-              'n': nx * ny,
+              'n': n,
               'NeighborE': 4,
               'NeighborW': 0,
               'NeighborN': 2,
@@ -25,7 +26,7 @@ def simple_mesh(nx, ny):
               'SW': [0, 5],
               'nx': nx,
               'ny': ny,
-              'n': nx * ny,
+              'n': n,
               'NeighborE': 3,
               'NeighborW': 0,
               'NeighborN': 0,
@@ -42,7 +43,7 @@ def simple_mesh(nx, ny):
               'SW': [5, 5],
               'nx': nx,
               'ny': ny,
-              'n': nx * ny,
+              'n': n,
               'NeighborE': 0,
               'NeighborW': 2,
               'NeighborN': 0,
@@ -59,7 +60,7 @@ def simple_mesh(nx, ny):
               'SW': [5, 0],
               'nx': nx,
               'ny': ny,
-              'n': nx * ny,
+              'n': n,
               'NeighborE': 0,
               'NeighborW': 1,
               'NeighborN': 3,
@@ -69,13 +70,63 @@ def simple_mesh(nx, ny):
               'BCTypeN': 'None',
               'BCTypeS': 'Reflection'}
 
-    return {1: block1,
-            2: block2,
-            3: block3,
-            4: block4}
+    return mesh_builder.make_mesh_inputs(block1, block2, block3, block4)
 
+def three_mesh(nx, ny, n):
+    block1 = {'nBLK': 1,
+              'NE': [5, 5],
+              'NW': [0, 5],
+              'SE': [5, 0],
+              'SW': [0, 0],
+              'nx': nx,
+              'ny': ny,
+              'n': n,
+              'NeighborE': 0,
+              'NeighborW': 0,
+              'NeighborN': 2,
+              'NeighborS': 0,
+              'BCTypeE': 'Reflection',
+              'BCTypeW': 'Reflection',
+              'BCTypeN': 'None',
+              'BCTypeS': 'Reflection'}
 
-def one_mesh(nx, ny):
+    block2 = {'nBLK': 2,
+              'NE': [5, 10],
+              'NW': [0, 10],
+              'SE': [5, 5],
+              'SW': [0, 5],
+              'nx': nx,
+              'ny': ny,
+              'n': n,
+              'NeighborE': 3,
+              'NeighborW': 0,
+              'NeighborN': 0,
+              'NeighborS': 1,
+              'BCTypeE': 'None',
+              'BCTypeW': 'Reflection',
+              'BCTypeN': 'Reflection',
+              'BCTypeS': 'None'}
+
+    block3 = {'nBLK': 3,
+              'NE': [10, 10],
+              'NW': [5, 10],
+              'SE': [10, 5],
+              'SW': [5, 5],
+              'nx': nx,
+              'ny': ny,
+              'n': n,
+              'NeighborE': 0,
+              'NeighborW': 2,
+              'NeighborN': 0,
+              'NeighborS': 0,
+              'BCTypeE': 'Reflection',
+              'BCTypeW': 'None',
+              'BCTypeN': 'Reflection',
+              'BCTypeS': 'Reflection'}
+
+    return mesh_builder.make_mesh_inputs(block1, block2, block3)
+
+def one_mesh(n, nx, ny):
     block1 = {'nBLK': 1,
               'NE': [10, 10],
               'NW': [0, 10],
@@ -83,7 +134,7 @@ def one_mesh(nx, ny):
               'SW': [0, 0],
               'nx': nx,
               'ny': ny,
-              'n': nx * ny,
+              'n': n,
               'NeighborE': 0,
               'NeighborW': 0,
               'NeighborN': 0,
@@ -93,10 +144,4 @@ def one_mesh(nx, ny):
               'BCTypeN': 'Reflection',
               'BCTypeS': 'Reflection'}
 
-    return {1: block1}
-
-
-DEFINED_MESHES = {'simple_mesh': simple_mesh,
-                  'one_mesh': one_mesh}
-
-
+    return mesh_builder.make_mesh_inputs(block1)
