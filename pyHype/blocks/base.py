Fixed residual computation for general quadrilateral
Now uses the midpoint quadrature integration method everywhere

pyHype/blocks/base.py
@@ -297,12 +297,15 @@ class QuadBlock:
         self.mesh.EW_midpoint_x, self.mesh.EW_midpoint_y = self.mesh.get_EW_face_midpoint()
         self.mesh.NS_midpoint_x, self.mesh.NS_midpoint_y = self.mesh.get_NS_face_midpoint()
 
+        self.mesh.E_face_L = self.mesh.east_face_length()[:, :, np.newaxis]
+        self.mesh.W_face_L = self.mesh.west_face_length()[:, :, np.newaxis]
+        self.mesh.N_face_L = self.mesh.north_face_length()[:, :, np.newaxis]
+        self.mesh.S_face_L = self.mesh.south_face_length()[:, :, np.newaxis]
+
         self.mesh.dx = self.mesh.EW_midpoint_x[:, 1:, :] - self.mesh.EW_midpoint_x[:, :-1, :]
         self.mesh.dy = self.mesh.NS_midpoint_y[1:, :, :] - self.mesh.NS_midpoint_y[:-1, :, :]
 
-        # --------------------------------------------------------------------------------------------------------------
-        # DEBUGGING
-
+    def plot(self):
         plt.scatter(self.mesh.x, self.mesh.y, color='black', s=15)
         plt.scatter(self.mesh.xc, self.mesh.yc, color='mediumslateblue', s=15)
 
@@ -313,8 +316,11 @@ class QuadBlock:
 
         segs1 = np.stack((self.mesh.xc, self.mesh.yc), axis=2)
         segs2 = segs1.transpose((1, 0, 2))
-        plt.gca().add_collection(LineCollection(segs1, colors='mediumslateblue', linestyles='--', linewidths=1, alpha=0.5))
-        plt.gca().add_collection(LineCollection(segs2, colors='mediumslateblue', linestyles='--', linewidths=1, alpha=0.5))
+
+        plt.gca().add_collection(
+            LineCollection(segs1, colors='mediumslateblue', linestyles='--', linewidths=1, alpha=0.5))
+        plt.gca().add_collection(
+            LineCollection(segs2, colors='mediumslateblue', linestyles='--', linewidths=1, alpha=0.5))
 
         plt.scatter(self.ghost.E.x, self.ghost.E.y, marker='s', color='black', s=15)
         plt.scatter(self.ghost.W.x, self.ghost.W.y, marker='s', color='black', s=15)
@@ -324,21 +330,16 @@ class QuadBlock:
         plt.scatter(self.mesh.EW_midpoint_x[:, :, 0], self.mesh.EW_midpoint_y[:, :, 0], marker='^', color='red', s=15)
         plt.scatter(self.mesh.NS_midpoint_x[:, :, 0], self.mesh.NS_midpoint_y[:, :, 0], marker='x', color='green', s=15)
 
-        #plt.scatter(self.mesh.xc[1:, :], self.mesh.yc[1:, :], marker='^', color='red', s=15)
-
         plt.gca().set_aspect('equal', adjustable='box')
         plt.show()
-        #plt.pause(100)
-
-        # --------------------------------------------------------------------------------------------------------------
 
     @property
-    def Flux_X(self):
-        return self.fvm.Flux_X
+    def Flux_EW(self):
+        return self.fvm.Flux_EW
 
     @property
-    def Flux_Y(self):
-        return self.fvm.Flux_Y
+    def Flux_NS(self):
+        return self.fvm.Flux_NS
 
     def __getitem__(self, index):
         y, x, var = index

pyHype/blocks/boundary.py
@@ -17,6 +17,7 @@ limitations under the License.
 import numpy as np
 from abc import abstractmethod
 from pyHype.states import ConservativeState
+from pyHype.utils import utils
 
 
 class GhostBlock:
@@ -79,88 +80,6 @@ class GhostBlock:
         pass
 
 
-class GhostBlockNorth(GhostBlock):
-    def __init__(self, inputs, type_, ref_BLK):
-        super().__init__(inputs, type_, ref_BLK)
-        self.state = ConservativeState(inputs, nx=self.nx, ny=self.nghost)
-
-        self.x = np.zeros((1, self.inputs.nx))
-        self.y = np.zeros((1, self.inputs.nx))
-
-        self.theta = ref_BLK.thetaN
-
-        x = self.ref_BLK.mesh.x[-1, :]
-        y = self.ref_BLK.mesh.y[-1, :]
-
-        dx = x - self.ref_BLK.mesh.x[-2, :]
-        dy = y - self.ref_BLK.mesh.y[-2, :]
-
-        self.x[0, :] = x + dx
-        self.y[0, :] = y + dy
-
-        cont_x = np.concatenate((self.x, ref_BLK.mesh.x[-1:, :]), axis=0)
-        cont_y = np.concatenate((self.y, ref_BLK.mesh.y[-1:, :]), axis=0)
-
-        xc, yc = ref_BLK.mesh.get_centroid(cont_x, cont_y)
-
-        self.ref_BLK.mesh.xc[-1, 1:-1] = xc
-        self.ref_BLK.mesh.yc[-1, 1:-1] = yc
-
-    def set_BC_none(self):
-        self.state.U = self.ref_BLK.neighbors.N.get_south_ghost()
-        self.state.set_vars_from_state()
-
-    def set_BC_outflow(self):
-        self.state.U[:, :, :] = self.ref_BLK.get_north_edge()
-        self.state.set_vars_from_state()
-
-    def set_BC_reflection(self):
-        self.state.U = self.ref_BLK.get_north_ghost()
-        self.state.U[:, :, 2] *= -1
-        self.state.set_vars_from_state()
-
-
-class GhostBlockSouth(GhostBlock):
-    def __init__(self, inputs, type_, ref_BLK):
-        super().__init__(inputs, type_, ref_BLK)
-        self.state = ConservativeState(inputs, nx=self.nx, ny=self.nghost)
-
-        self.x = np.zeros((1, self.inputs.nx))
-        self.y = np.zeros((1, self.inputs.nx))
-
-        self.theta = ref_BLK.thetaS
-
-        x = self.ref_BLK.mesh.x[0, :]
-        y = self.ref_BLK.mesh.y[0, :]
-
-        dx = x - self.ref_BLK.mesh.x[1, :]
-        dy = y - self.ref_BLK.mesh.y[1, :]
-
-        self.x[0, :] = x + dx
-        self.y[0, :] = y + dy
-
-        cont_x = np.concatenate((self.x, ref_BLK.mesh.x[0:1, :]), axis=0)
-        cont_y = np.concatenate((self.y, ref_BLK.mesh.y[0:1, :]), axis=0)
-
-        xc, yc = ref_BLK.mesh.get_centroid(cont_x, cont_y)
-
-        self.ref_BLK.mesh.xc[0, 1:-1] = xc
-        self.ref_BLK.mesh.yc[0, 1:-1] = yc
-
-    def set_BC_none(self):
-        self.state.U = self.ref_BLK.neighbors.S.get_north_ghost()
-        self.state.set_vars_from_state()
-
-    def set_BC_outflow(self):
-        self.state.U[:, :, :] = self.ref_BLK.get_south_edge()
-        self.state.set_vars_from_state()
-
-    def set_BC_reflection(self):
-        self.state.U = self.ref_BLK.get_south_ghost()
-        self.state.U[:, :, 2] *= -1
-        self.state.set_vars_from_state()
-
-
 class GhostBlockEast(GhostBlock):
     def __init__(self, inputs, type_, ref_BLK):
         super().__init__(inputs, type_, ref_BLK)
@@ -197,9 +116,11 @@ class GhostBlockEast(GhostBlock):
         self.state.set_vars_from_state()
 
     def set_BC_reflection(self):
-        self.state.U = self.ref_BLK.get_east_ghost()
-        self.state.U[:, :, 1] *= -1
-        self.state.set_vars_from_state()
+        state = self.ref_BLK.get_east_ghost()
+        utils.rotate(state, self.ref_BLK.mesh.thetax[-1])
+        state[:, :, 1] *= -1
+
+        self.state.update(state)
 
 
 class GhostBlockWest(GhostBlock):
@@ -238,6 +159,91 @@ class GhostBlockWest(GhostBlock):
         self.state.set_vars_from_state()
 
     def set_BC_reflection(self):
-        self.state.U = self.ref_BLK.get_west_ghost()
-        self.state.U[:, :, 1] *= -1
+        state = self.ref_BLK.get_west_ghost()
+        utils.rotate(state, self.ref_BLK.mesh.thetax[0])
+        state[:, :, 1] *= -1
+        self.state.update(state)
+
+
+class GhostBlockNorth(GhostBlock):
+    def __init__(self, inputs, type_, ref_BLK):
+        super().__init__(inputs, type_, ref_BLK)
+        self.state = ConservativeState(inputs, nx=self.nx, ny=self.nghost)
+
+        self.x = np.zeros((1, self.inputs.nx))
+        self.y = np.zeros((1, self.inputs.nx))
+
+        self.theta = ref_BLK.thetaN
+
+        x = self.ref_BLK.mesh.x[-1, :]
+        y = self.ref_BLK.mesh.y[-1, :]
+
+        dx = x - self.ref_BLK.mesh.x[-2, :]
+        dy = y - self.ref_BLK.mesh.y[-2, :]
+
+        self.x[0, :] = x + dx
+        self.y[0, :] = y + dy
+
+        cont_x = np.concatenate((self.x, ref_BLK.mesh.x[-1:, :]), axis=0)
+        cont_y = np.concatenate((self.y, ref_BLK.mesh.y[-1:, :]), axis=0)
+
+        xc, yc = ref_BLK.mesh.get_centroid(cont_x, cont_y)
+
+        self.ref_BLK.mesh.xc[-1, 1:-1] = xc
+        self.ref_BLK.mesh.yc[-1, 1:-1] = yc
+
+    def set_BC_none(self):
+        self.state.U = self.ref_BLK.neighbors.N.get_south_ghost()
         self.state.set_vars_from_state()
+
+    def set_BC_outflow(self):
+        self.state.U[:, :, :] = self.ref_BLK.get_north_edge()
+        self.state.set_vars_from_state()
+
+    def set_BC_reflection(self):
+        state = self.ref_BLK.get_north_ghost()
+        utils.rotate(state, np.pi / 2 - self.ref_BLK.mesh.thetay[-1])
+        state[:, :, 2] *= -1
+        self.state.update(state)
+
+
+class GhostBlockSouth(GhostBlock):
+    def __init__(self, inputs, type_, ref_BLK):
+        super().__init__(inputs, type_, ref_BLK)
+        self.state = ConservativeState(inputs, nx=self.nx, ny=self.nghost)
+
+        self.x = np.zeros((1, self.inputs.nx))
+        self.y = np.zeros((1, self.inputs.nx))
+
+        self.theta = ref_BLK.thetaS
+
+        x = self.ref_BLK.mesh.x[0, :]
+        y = self.ref_BLK.mesh.y[0, :]
+
+        dx = x - self.ref_BLK.mesh.x[1, :]
+        dy = y - self.ref_BLK.mesh.y[1, :]
+
+        self.x[0, :] = x + dx
+        self.y[0, :] = y + dy
+
+        cont_x = np.concatenate((self.x, ref_BLK.mesh.x[0:1, :]), axis=0)
+        cont_y = np.concatenate((self.y, ref_BLK.mesh.y[0:1, :]), axis=0)
+
+        xc, yc = ref_BLK.mesh.get_centroid(cont_x, cont_y)
+
+        self.ref_BLK.mesh.xc[0, 1:-1] = xc
+        self.ref_BLK.mesh.yc[0, 1:-1] = yc
+
+    def set_BC_none(self):
+        self.state.U = self.ref_BLK.neighbors.S.get_north_ghost()
+        self.state.set_vars_from_state()
+
+    def set_BC_outflow(self):
+        self.state.U[:, :, :] = self.ref_BLK.get_south_edge()
+        self.state.set_vars_from_state()
+
+    def set_BC_reflection(self):
+        state = self.ref_BLK.get_south_ghost()
+        utils.rotate(state, np.pi / 2 - self.ref_BLK.mesh.thetay[0])
+        state[:, :, 2] *= -1
+        self.state.update(state)

pyHype/fvm/Gradients/Gradients.py
@@ -62,26 +62,22 @@ class GreenGauss:
         # Concatenate mesh state and ghost block states
         interfaceEW, interfaceNS = refBLK.get_interface_values(self.inputs.reconstruction_type)
 
-        # Calculate Side Length
-        lengthE = refBLK.mesh.east_side_length()[:, :, np.newaxis]
-        lengthW = refBLK.mesh.west_side_length()[:, :, np.newaxis]
-        lengthN = refBLK.mesh.north_side_length()[:, :, np.newaxis]
-        lengthS = refBLK.mesh.south_side_length()[:, :, np.newaxis]
-
         # Get each face's contribution to dUdx
-        E = interfaceEW[:, 1:, :] * refBLK.mesh.EW_norm_x[0:, 1:, np.newaxis] * lengthE
-        W = interfaceEW[:, :-1, :] * refBLK.mesh.EW_norm_x[0:, :-1, np.newaxis] * lengthW * (-1)
-        N = interfaceNS[1:, :, :] * refBLK.mesh.NS_norm_x[1:, 0:, np.newaxis] * lengthN
-        S = interfaceNS[:-1, :, :] * refBLK.mesh.NS_norm_x[:-1, 0:, np.newaxis] * lengthS * (-1)
+        E = interfaceEW[:, 1:, :] * refBLK.mesh.E_face_L
+        W = interfaceEW[:, :-1, :] * refBLK.mesh.W_face_L * (-1)
+        N = interfaceNS[1:, :, :] * refBLK.mesh.N_face_L
+        S = interfaceNS[:-1, :, :] * refBLK.mesh.S_face_L * (-1)
 
         # Compute dUdx
-        refBLK.gradx = (E + W + N + S) / refBLK.mesh.A[:, :, np.newaxis]
-
-        # Get each face's contribution to dUdy
-        E = interfaceEW[:, 1:, :] * refBLK.mesh.EW_norm_y[0:, 1:, np.newaxis] * lengthE
-        W = interfaceEW[:, :-1, :] * refBLK.mesh.EW_norm_y[0:, :-1, np.newaxis] * lengthW * (-1)
-        N = interfaceNS[1:, :, :] * refBLK.mesh.NS_norm_y[1:, 0:, np.newaxis] * lengthN
-        S = interfaceNS[:-1, :, :] * refBLK.mesh.NS_norm_y[:-1, 0:, np.newaxis] * lengthS * (-1)
+        refBLK.gradx = (E * refBLK.mesh.EW_norm_x[0:, 1:, np.newaxis]  +
+                        W * refBLK.mesh.EW_norm_x[0:, :-1, np.newaxis] +
+                        N * refBLK.mesh.NS_norm_x[1:, 0:, np.newaxis]  +
+                        S * refBLK.mesh.NS_norm_x[:-1, 0:, np.newaxis]
+                        ) / refBLK.mesh.A
 
         # Compute dUdy
-        refBLK.grady = (E + W + N + S) / refBLK.mesh.A[:, :, np.newaxis]
+        refBLK.grady = (E * refBLK.mesh.EW_norm_y[0:, 1:, np.newaxis] +
+                        W * refBLK.mesh.EW_norm_y[0:, :-1, np.newaxis] +
+                        N * refBLK.mesh.NS_norm_y[1:, 0:, np.newaxis] +
+                        S * refBLK.mesh.NS_norm_y[:-1, 0:, np.newaxis]
+                        ) / refBLK.mesh.A

pyHype/fvm/base.py
@@ -148,8 +148,8 @@ class MUSCLFiniteVolumeMethod:
         self.global_nBLK = global_nBLK
 
         # Initialize x and y direction flux
-        self.Flux_X = np.empty((self.ny, self.nx, 4))
-        self.Flux_Y = np.empty((self.ny, self.nx, 4))
+        self.Flux_EW = np.empty((self.ny, self.nx + 1, 4))
+        self.Flux_NS = np.empty((self.ny + 1, self.nx, 4))
 
         # Initialize left and right conservative states
         self.UL = ConservativeState(self.inputs, nx=self.nx + 1, ny=1)

pyHype/mesh/base.py
@@ -16,6 +16,8 @@ limitations under the License.
 
 import numpy as np
 from typing import Union
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 
 
 class BlockDescription:
@@ -71,13 +73,6 @@ class Mesh:
         self.xc = np.zeros((self.ny + 1, self.nx + 1))
         self.yc = np.zeros((self.ny + 1, self.nx + 1))
 
-        self.A = np.zeros((self.ny, self.nx))
-
-        self.create_mesh()
-
-        self.Lx    = self.vertices.NE[0] - self.vertices.NW[0]
-        self.Ly    = self.vertices.NE[1] - self.vertices.SE[1]
-
         self.EW_norm_x = np.zeros((1, self.nx + 1))
         self.EW_norm_y = np.zeros((1, self.nx + 1))
 
@@ -87,10 +82,19 @@ class Mesh:
         self.thetax = np.zeros((self.nx + 1))
         self.thetay = np.zeros((self.ny + 1))
 
+        self.E_face_L = None
+        self.W_face_L = None
+        self.N_face_L = None
+        self.S_face_L = None
+
         self.EW_midpoint_x = None
         self.EW_midpoint_y = None
         self.NS_midpoint_x = None
         self.NS_midpoint_y = None
+
+        self.A = np.zeros((self.ny, self.nx))
+
+        self.create_mesh()
 
     def create_mesh(self):
 
@@ -211,10 +215,10 @@ class Mesh:
         """
 
         # Side lengths
-        s1 = self.west_side_length()
-        s3 = self.east_side_length()
-        s2 = self.north_side_length()
-        s4 = self.south_side_length()
+        s1 = self.west_face_length()
+        s3 = self.east_face_length()
+        s2 = self.north_face_length()
+        s4 = self.south_face_length()
 
         # Diagonal
         d1 = (self.xc[1:, :-1] - self.xc[:-1, 1:]) ** 2 + \
@@ -231,7 +235,7 @@ class Mesh:
         p1 = (s - s1) * (s - s2) * (s - s3) * (s - s4)
         p2 = s1 * s2 * s3 * s4
 
-        self.A = np.sqrt(p1 - 0.5 * p2 * (1 + np.cos(a1 + a2)))
+        self.A = np.sqrt(p1 - 0.5 * p2 * (1 + np.cos(a1 + a2)))[:, :, np.newaxis]
 
     @staticmethod
     def get_centroid(x: np.ndarray, y: np.ndarray):
@@ -244,27 +248,19 @@ class Mesh:
 
         return xc, yc
 
-    def east_side_length(self):
-        """print('-----------------------------------------------')
-        print(self.x)
-        print(self.y)
-        print('-----------------------------------------------')
-        print(self.xc)
-        print(self.yc)
-        print(self.yc[1:, 1:])
-        print(self.yc[:-1, 1:])"""
+    def east_face_length(self):
         return np.sqrt(((self.xc[1:, 1:] - self.xc[:-1, 1:]) ** 2 +
                         (self.yc[1:, 1:] - self.yc[:-1, 1:]) ** 2))
 
-    def west_side_length(self):
+    def west_face_length(self):
         return np.sqrt(((self.xc[1:, :-1] - self.xc[:-1, :-1]) ** 2 +
                         (self.yc[1:, :-1] - self.yc[:-1, :-1]) ** 2))
 
-    def north_side_length(self):
+    def north_face_length(self):
         return np.sqrt(((self.xc[1:, :-1] - self.xc[1:, 1:]) ** 2 +
                         (self.yc[1:, :-1] - self.yc[1:, 1:]) ** 2))
 
-    def south_side_length(self):
+    def south_face_length(self):
         return np.sqrt(((self.xc[:-1, :-1] - self.xc[:-1, 1:]) ** 2 +
                         (self.yc[:-1, :-1] - self.yc[:-1, 1:]) ** 2))
 

pyHype/utils/utils.py
@@ -16,9 +16,10 @@ limitations under the License.
 
 import numpy as np
 
-def rotate_row(array: np.ndarray,
-               theta: float
-               ) -> None:
+
+def rotate(array: np.ndarray,
+           theta: float
+           ) -> None:
     """
     Rotates a 1 * nx * 4 ndarray that represents a row of nodes from a State by theta degrees counterclockwise.
     The contents of the array may be Conservative/Primitive state variables, Fluxes, etc...
@@ -46,9 +47,9 @@ def rotate_row(array: np.ndarray,
     array[:, :, 2] = v
 
 
-def unrotate_row(array: np.ndarray,
-                 theta: float
-                 ) -> None:
+def unrotate(array: np.ndarray,
+             theta: float
+             ) -> None:
     """
     Rotates a 1 * nx * 4 ndarray that represents a row of nodes from a State by theta degrees clockwise. Basically the
     inverse of rotate_row(). The contents of the array may be Conservative/Primitive state variables, Fluxes, etc...