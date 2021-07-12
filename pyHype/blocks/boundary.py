4d8b72bb95cd51a976b4cbe0937013b8be5566f8
Mohamed Khalil <62818656+momokhalil@users.noreply.github.com>
2021-07-12 12:20:41 -0400

    Added comments to ghost block constructors

--- pyHype/blocks/boundary.py
+++ pyHype/blocks/boundary.py
@@ -24,7 +24,10 @@ from pyHype.mesh.base import Mesh
 
 
 class GhostBlock:
-    def __init__(self, inputs, BCtype: str, refBLK):
+    def __init__(self,
+                 inputs,
+                 BCtype: str,
+                 refBLK):
 
         self.BCtype = BCtype
         self.inputs = inputs
@@ -35,10 +38,6 @@ class GhostBlock:
 
         self.state = None
         self.theta = None
-        self.x = None
-        self.y = None
-        self.xc = None
-        self.yc = None
 
         self.mesh = None
 
@@ -68,29 +67,38 @@ class GhostBlock:
 
 
 class GhostBlockEast(GhostBlock):
-    def __init__(self, inputs, BCtype, refBLK):
+    """
 
+    """
+
+    def __init__(self,
+                 inputs,
+                 BCtype,
+                 refBLK):
+
+        # Call superclass contructor
         super().__init__(inputs, BCtype, refBLK)
 
+        # Construct ConservativeState class to represent the solution on the mesh
         self.state = ConservativeState(inputs, nx=self.nghost, ny=self.ny)
 
-        self.x = np.zeros((self.inputs.ny + 1, 1))
-        self.y = np.zeros((self.inputs.ny + 1, 1))
-
+        # Set geometric angle
         self.theta = refBLK.thetaE
 
+        # Calculate location of NorthWest corner
         NWx = self.refBLK.mesh.nodes.x[-1, -1]
         NWy = self.refBLK.mesh.nodes.y[-1, -1]
-
+        # Calculate location of SouthWest corner
         SWx = self.refBLK.mesh.nodes.x[0, -1]
         SWy = self.refBLK.mesh.nodes.y[0, -1]
-
+        # Calculate x-direction and y-direction distance between NorthWest and NorthEast corner
         dx_NE = NWx - self.refBLK.mesh.nodes.x[-1, -1 - self.inputs.nghost]
         dy_NE = NWy - self.refBLK.mesh.nodes.y[-1, -1 - self.inputs.nghost]
-
+        # Calculate x-direction and y-direction distance between SouthWest and SouthEast corner
         dx_SE = SWx - self.refBLK.mesh.nodes.x[0, -1 - self.inputs.nghost]
         dy_SE = SWy - self.refBLK.mesh.nodes.y[0, -1 - self.inputs.nghost]
 
+        # Construct Mesh
         self.mesh = Mesh(self.inputs,
                          NE=(NWx + dx_NE, NWy - dy_NE),
                          NW=(NWx, NWy),
@@ -117,29 +125,34 @@ class GhostBlockEast(GhostBlock):
 
 
 class GhostBlockWest(GhostBlock):
-    def __init__(self, inputs, BCtype, refBLK):
+    def __init__(self,
+                 inputs,
+                 BCtype,
+                 refBLK):
 
+        # Call superclass contructor
         super().__init__(inputs, BCtype, refBLK)
 
+        # Construct ConservativeState class to represent the solution on the mesh
         self.state = ConservativeState(inputs, nx=self.nghost, ny=self.ny)
 
-        self.x = np.zeros((self.inputs.ny + 1, 1))
-        self.y = np.zeros((self.inputs.ny + 1, 1))
-
+        # Set geometric angle
         self.theta = refBLK.thetaW
 
+        # Calculate location of NorthEast corner
         NEx = self.refBLK.mesh.nodes.x[-1, 0]
         NEy = self.refBLK.mesh.nodes.y[-1, 0]
-
+        # Calculate location of SouthEast corner
         SEx = self.refBLK.mesh.nodes.x[0, 0]
         SEy = self.refBLK.mesh.nodes.y[0, 0]
-
+        # Calculate x-direction and y-direction distance between NorthWest and NorthEast corner
         dx_NW = NEx - self.refBLK.mesh.nodes.x[-1, self.inputs.nghost]
         dy_NW = NEy - self.refBLK.mesh.nodes.y[-1, self.inputs.nghost]
-
+        # Calculate x-direction and y-direction distance between SouthWest and SouthEast corner
         dx_SW = SEx - self.refBLK.mesh.nodes.x[0, self.inputs.nghost]
         dy_SW = SEy - self.refBLK.mesh.nodes.y[0, self.inputs.nghost]
 
+        # Construct Mesh
         self.mesh = Mesh(self.inputs,
                          NE=(NEx, NEy),
                          NW=(NEx + dx_NW, NEy - dy_NW),
@@ -166,29 +179,34 @@ class GhostBlockWest(GhostBlock):
 
 
 class GhostBlockNorth(GhostBlock):
-    def __init__(self, inputs, BCtype, refBLK):
+    def __init__(self,
+                 inputs,
+                 BCtype,
+                 refBLK):
 
+        # Call superclass contructor
         super().__init__(inputs, BCtype, refBLK)
 
+        # Construct ConservativeState class to represent the solution on the mesh
         self.state = ConservativeState(inputs, nx=self.nx, ny=self.nghost)
 
-        self.x = np.zeros((1, self.inputs.nx + 1))
-        self.y = np.zeros((1, self.inputs.nx + 1))
-
+        # Set geometric angle
         self.theta = refBLK.thetaN
 
+        # Calculate location of SouthWest corner
         SWx = self.refBLK.mesh.nodes.x[-1, 0]
         SWy = self.refBLK.mesh.nodes.y[-1, 0]
-
+        # Calculate location of SouthEast corner
         SEx = self.refBLK.mesh.nodes.x[-1, -1]
         SEy = self.refBLK.mesh.nodes.y[-1, -1]
-
+        # Calculate x-direction and y-direction distance between NorthWest and SouthWest corner
         dx_NW = SWx - self.refBLK.mesh.nodes.x[-1 - self.inputs.nghost, 0]
         dy_NW = SWy - self.refBLK.mesh.nodes.y[-1 - self.inputs.nghost, 0]
-
+        # Calculate x-direction and y-direction distance between NorthEast and SouthEast corner
         dx_NE = SEx - self.refBLK.mesh.nodes.x[-1 - self.inputs.nghost, -1]
         dy_NE = SEy - self.refBLK.mesh.nodes.y[-1 - self.inputs.nghost, -1]
 
+        # Construct Mesh
         self.mesh = Mesh(self.inputs,
                          NE=(SEx - dx_NE, SEy + dy_NE),
                          NW=(SWx - dx_NW, SWy + dy_NW),
@@ -214,29 +232,32 @@ class GhostBlockNorth(GhostBlock):
 
 
 class GhostBlockSouth(GhostBlock):
-    def __init__(self, inputs, BCtype, refBLK):
+    def __init__(self,
+                 inputs,
+                 BCtype,
+                 refBLK):
 
+        # Call superclass contructor
         super().__init__(inputs, BCtype, refBLK)
-        
-        self.state = ConservativeState(inputs, nx=self.nx, ny=self.nghost)
 
-        self.x = np.zeros((1, self.inputs.nx + 1))
-        self.y = np.zeros((1, self.inputs.nx + 1))
+        self.state = ConservativeState(inputs, nx=self.nx, ny=self.nghost)
 
         self.theta = refBLK.thetaS
 
+        # Calculate location of NorthWest corner
         NWx = self.refBLK.mesh.nodes.x[0, 0]
         NWy = self.refBLK.mesh.nodes.y[0, 0]
-
+        # Calculate location of NorthEast corner
         NEx = self.refBLK.mesh.nodes.x[0, -1]
         NEy = self.refBLK.mesh.nodes.y[0, -1]
-
+        # Calculate x-direction and y-direction distance between NorthWest and SouthWest corner
         dx_SW = NWx - self.refBLK.mesh.nodes.x[self.inputs.nghost, 0]
         dy_SW = NWy - self.refBLK.mesh.nodes.y[self.inputs.nghost, 0]
-
+        # Calculate x-direction and y-direction distance between NorthEast and SouthEast corner
         dx_SE = NEx - self.refBLK.mesh.nodes.x[self.inputs.nghost, -1]
         dy_SE = NEy - self.refBLK.mesh.nodes.y[self.inputs.nghost, -1]
 
+        # Construct Mesh
         self.mesh = Mesh(self.inputs,
                          NE=(NEx, NEy),
                          NW=(NWx, NWy),
