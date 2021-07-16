Fixed issues with rotations for flux calculation and wall reflection BCs
Angles were previously set incorrectly, and the functions for returning the state vectors used for constructing the ghost blocks were still using indexing for node-based formulations.

pyHype/blocks/base.py
@@ -468,7 +468,7 @@ class QuadBlock:
                                    ........................
         """
 
-        return self.state.U[:, -(self.mesh.nghost + 1):-1, :].copy()
+        return self.state.U[:, -self.mesh.nghost:, :].copy()
 
     def get_west_ghost(self) -> np.ndarray:
         """
@@ -540,7 +540,7 @@ class QuadBlock:
                         .......................
         """
 
-        return self.state.U[:, 1:(self.mesh.nghost + 1), :].copy()
+        return self.state.U[:, :self.mesh.nghost, :].copy()
 
     def get_north_ghost(self) -> np.ndarray:
         """
@@ -611,7 +611,7 @@ class QuadBlock:
                   O----------O----------O----------O----------O
         """
 
-        return self.state.U[-(self.mesh.nghost + 1):-1, :, :].copy()
+        return self.state.U[-self.mesh.nghost:, :, :].copy()
 
     def get_south_ghost(self) -> np.ndarray:
         """
@@ -682,7 +682,7 @@ class QuadBlock:
                   O----------O----------O----------O----------O
         """
 
-        return self.state.U[1:(self.mesh.nghost + 1), :, :].copy()
+        return self.state.U[:self.mesh.nghost, :, :].copy()
 
     def get_east_edge(self) -> np.ndarray:
         """

pyHype/blocks/ghost.py
@@ -145,14 +145,7 @@ class GhostBlockEast(GhostBlock):
         # Get wall angle
         wall_angle = self.refBLK.mesh.get_east_face_angle()
         # Rotate state to allign with wall
-        """for i in range(self.refBLK.mesh.nghost):
-            print(state[:, i, None])
-            utils.rotate(wall_angle, state[:, i, None])
-
-        states = [state[:, i, None] for i in range(self.nghost)]
-        utils.rotate(wall_angle, *states)"""
-        # Rotate state to allign with wall
-        map(utils.rotate, wall_angle, [state[:, i, None] for i in range(self.nghost)])
+        utils.rotate(wall_angle, state)
         # Reflect normal velocity
         state[:, :, 1] *= -1
         # Update state
@@ -209,15 +202,10 @@ class GhostBlockWest(GhostBlock):
     def set_BC_reflection(self):
         # Get state from interior domain
         state = self.refBLK.get_west_ghost()
-        """utils.rotate(self.refBLK.mesh.thetax[0], state)
-        state[:, :, 1] *= -1
-        self.state.update(state)"""
         # Get wall angle
         wall_angle = self.refBLK.mesh.get_west_face_angle()
         # Rotate state to allign with wall
-        """for i in range(self.nghost):
-            utils.rotate(wall_angle, state[:, i, None])"""
-        map(utils.rotate, wall_angle, [state[:, i, None] for i in range(self.nghost)])
+        utils.rotate(wall_angle, state)
         # Reflect normal velocity
         state[:, :, 1] *= -1
         # Update state
@@ -274,13 +262,15 @@ class GhostBlockNorth(GhostBlock):
         self.state.set_vars_from_state()
 
     def set_BC_reflection(self):
+        # Get state from interior domain
         state = self.refBLK.get_north_ghost()
-        wall_angle = np.pi / 2 - self.refBLK.mesh.get_north_face_angle()
-
-        map(utils.rotate, wall_angle, [state[i, :, None] for i in range(self.nghost)])
-
-        #utils.rotate(np.pi / 2 - self.refBLK.mesh.thetay[-1], state)
+        # Get wall angle
+        wall_angle = self.refBLK.mesh.get_north_face_angle()
+        # Rotate state to allign with wall
+        utils.rotate(wall_angle - np.pi/2, state)
+        # Reflect normal velocity
         state[:, :, 2] *= -1
+        # Update state
         self.state.update(state)
 
 
@@ -332,13 +322,13 @@ class GhostBlockSouth(GhostBlock):
         self.state.set_vars_from_state()
 
     def set_BC_reflection(self):
+        # Get state from interior domain
         state = self.refBLK.get_south_ghost()
-        #utils.rotate(np.pi / 2 - self.refBLK.mesh.thetay[0], state)
-
-        wall_angle = np.pi / 2 - self.refBLK.mesh.get_south_face_angle()
-
-        map(utils.rotate, wall_angle, [state[i, :, None] for i in range(self.nghost)])
-
-
+        # Get wall angle
+        wall_angle = self.refBLK.mesh.get_south_face_angle()
+        # Rotate state to allign with wall
+        utils.rotate(wall_angle - np.pi/2, state)
+        # Reflect normal velocity
         state[:, :, 2] *= -1
+        # Update state
         self.state.update(state)

pyHype/fvm/base.py
@@ -358,14 +358,14 @@ class MUSCLFiniteVolumeMethod:
         self.UR.reset(shape=(1, self.nx + 1, 4))
 
         # Copy all ghost cell values that will be used for the flux calculations
-        _east_ghost = refBLK.ghost.W.col_copy(0)
+        _east_ghost = refBLK.ghost.E.col_copy(0)
         _west_ghost = refBLK.ghost.W.col_copy(-1)
         _north_ghost = refBLK.ghost.N.row_copy(0)
         _south_ghost = refBLK.ghost.S.row_copy(-1)
 
         # Rotate to allign with cell faces
         utils.rotate(refBLK.mesh.get_east_face_angle(), _east_ghost)
-        utils.rotate(refBLK.mesh.get_west_face_angle() - np.pi, _west_ghost)
+        utils.rotate(refBLK.mesh.get_west_face_angle(), _west_ghost)
         utils.rotate(refBLK.mesh.faceE.theta, stateE)
         utils.rotate(refBLK.mesh.faceW.theta - np.pi, stateW)
 
@@ -373,11 +373,11 @@ class MUSCLFiniteVolumeMethod:
         for row in range(self.ny):
 
             # Set vectors based on left and right states
-            stateL = np.concatenate((_east_ghost[row:row+1, :, :],
+            stateL = np.concatenate((_west_ghost[row:row+1, :, :],
                                      stateE[row:row+1, :, :]), axis=1)
 
             stateR = np.concatenate((stateW[row:row+1, :, :],
-                                     _west_ghost[row:row+1, :, :]), axis=1)
+                                     _east_ghost[row:row+1, :, :]), axis=1)
 
             self.UL.from_conservative_state_vector(stateL)
             self.UR.from_conservative_state_vector(stateR)
@@ -403,7 +403,7 @@ class MUSCLFiniteVolumeMethod:
 
         # Rotate to allign with cell faces
         utils.rotate(refBLK.mesh.get_north_face_angle(), _north_ghost)
-        utils.rotate(refBLK.mesh.get_south_face_angle() - np.pi, _south_ghost)
+        utils.rotate(refBLK.mesh.get_south_face_angle(), _south_ghost)
         utils.rotate(refBLK.mesh.faceN.theta, stateN)
         utils.rotate(refBLK.mesh.faceS.theta - np.pi, stateS)
 