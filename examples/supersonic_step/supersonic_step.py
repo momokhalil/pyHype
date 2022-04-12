e4f686854c6f67d7b65954653b8c9dc14167986a
Mohamed Khalil <62818656+momokhalil@users.noreply.github.com>
2022-04-12 02:40:52 -0400

    Update supersonic_step.py
    Update example file to run with updated code

--- examples/supersonic_step/supersonic_step.py
+++ examples/supersonic_step/supersonic_step.py
@@ -6,20 +6,20 @@ mesh = step_ten_block()
 # Solver settings
 settings = {'problem_type':             'supersonic_flood',
             'interface_interpolation':  'arithmetic_average',
-            'reconstruction_type':      'primitive',
+            'reconstruction_type':      'conservative',
             'upwind_mode':              'primitive',
             'write_solution':           False,
             'write_solution_mode':      'every_n_timesteps',
             'write_solution_name':      'nozzle',
-            'write_every_n_timesteps':  10,
-            'plot_every':               10,
-            'CFL':                      0.3,
-            't_final':                  50.0,
+            'write_every_n_timesteps':  15,
+            'plot_every':               1,
+            'CFL':                      0.01,
+            't_final':                  0.1,
             'realplot':                 True,
             'profile':                  False,
             'gamma':                    1.4,
             'rho_inf':                  1.0,
-            'a_inf':                    1.0,
+            'a_inf':                    343.0,
             'R':                        287.0,
             'nx':                       45,
             'ny':                       15,
@@ -31,13 +31,15 @@ settings = {'problem_type':             'supersonic_flood',
             }
 
 # Create solver
-exp = Euler2D(fvm='SecondOrderPWL',
-              gradient='GreenGauss',
-              flux_function='HLLL',
-              limiter='Venkatakrishnan',
-              integrator='RK2',
+exp = Euler2D(fvm_type='MUSCL',
+              fvm_spatial_order=2,
+              fvm_num_quadrature_points=1,
+              fvm_gradient_type='GreenGauss',
+              fvm_flux_function='Roe',
+              fvm_slope_limiter='Venkatakrishnan',
+              time_integrator='RK2',
               settings=settings,
-              mesh=mesh)
+              mesh_inputs=mesh)
 
 # Solve
 exp.solve()
