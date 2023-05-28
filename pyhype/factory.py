27fcc3f41468223839f7549306534f4bf46b157b
Mohamed Khalil <62818656+momokhalil@users.noreply.github.com>
2023-05-28 00:58:53 -0400

    chore(remove unused imports)

--- pyhype/factory.py
+++ pyhype/factory.py
@@ -17,11 +17,6 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from pyhype.solvers.base import SolverConfig
-
 
 class Factory(ABC):
     @classmethod
