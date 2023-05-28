64d3af996d2f4f3fd5a8b0f1a791e82505e4cf73
Mohamed Khalil <62818656+momokhalil@users.noreply.github.com>
2023-05-27 20:14:50 -0400

    chore(remove explosion viz)

--- examples/explosion_multi/viz.py
+++
@@ -1,7 +0,0 @@
-from examples.explosion_multi.config import config
-from pyhype.utils.visualizer import Vizualizer
-
-
-if __name__ == "__main__":
-    viz = Vizualizer(config=config)
-    viz.plot(time_step=1600)
