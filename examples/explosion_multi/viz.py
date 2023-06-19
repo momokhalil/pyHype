9d69205f1de8af3fd736d4f4ec0390d459aba2c1
Mohamed Khalil <62818656+momokhalil@users.noreply.github.com>
2023-06-19 00:14:07 -0400

    chore(update viz)

--- examples/explosion_multi/viz.py
+++ examples/explosion_multi/viz.py
@@ -2,11 +2,12 @@ from pyhype.utils.visualizer import PlotSetup, Vizualizer
 from examples.explosion_multi.config import config
 
 setup = PlotSetup(
-    timesteps=[300, 400, 500],
+    timesteps=[100],
     size_x=10,
     size_y=10,
     x_lim=(0, 10),
     y_lim=(0, 20),
+    variable="density"
 )
 viz = Vizualizer(config=config)
 viz.plot(setup=setup)
